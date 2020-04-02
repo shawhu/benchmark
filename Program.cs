1b591e819d611ea175a207fd31f47f9186419259
Harry Xiao <shawhu@gmail.com>
2020-04-02 16:09:57 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace benchmark
+{
+    
+    class Program
+    {
+        static int length = 10000000;
+        static void Main(string[] args)
+        {
+            List<string> BigList = new List<string>();
+            Console.WriteLine("Start the benchmark");
+            Console.WriteLine($"Adding {length} string");
+            //create a big list
+            for (int i = 0; i < length; i++)
+            {
+                BigList.Add($"string{i}");
+            }
+            Console.WriteLine($"{length} string added");
+        }
+    }
+}
